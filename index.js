64acfeba7d5083cd4767ceaf09b23e81db0656ca
Gukson <714978398@qq.com>
2019-06-11 22:09:29 +0800

    Complete code

--- index.js
+++ index.js
@@ -3,14 +3,21 @@
 
 // 完成下面程序，将`name`变量中的字母全部转为为大写，输出：`'HELLO'`。
 var name = 'hello';
-// TODO write your code here...
-
+console.log(name.toUpperCase());
 
 // 完成下面程序，将sentence变量中的所有单词首字母变为大写，输出：'Good Afternoon, Mr Mike.'。
 var sentence = 'good afternoon, mr mike.';
-//TODO write your code here...
-
+function switchToUpperCase(str) {
+  let arr, newArr = [];
+  arr = str.toLowerCase().split(" ");
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(arr[i][0].toUpperCase() + arr[i].substring(1));
+  }
+  return newArr.join(" ");
+}
+console.log(switchToUpperCase(sentence));
 
 // 完成下面程序，将money变量中的金额提取出来，输出：20。
 var money = '￥20';
-//TODO write your code here...
+console.log(money.substr(1,2));
+console.log(money.substring(1));
